3833b6ac250e3e12899026645ff7a9be9ee70e23
lsysophia <59341273+lsysophia@users.noreply.github.com>
2020-09-27 19:49:20 -0500

    initial homepage styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -244,7 +244,7 @@ export default class Home extends Component {
                         </div>
                     </div>
                     :
-                    <div>
+                    <div className="home-nav">
                         <h1>Welcome to Rescue Match</h1>
                         <div className="user-nav">
                             {/* <div> */}
